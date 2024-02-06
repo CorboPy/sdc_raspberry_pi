78587640bd321d49419cb4bbe215f5a90c165d46
CorboPy <alex.corbett48@gmail.com>
2024-02-06 16:05:05 +0000

    update

--- funcs.py
+++ funcs.py
@@ -3,24 +3,25 @@
 
 # Single tcam request
 def get_tcam():
-    eight_by_eight_grid = np.zeros((8,8))   # Get most recent tcam image data (np.zeros placeholder, for testing purposes)
+    eight_by_eight_grid = 20*np.ones(8,8) + np.random.rand(8,8)   # Get most recent tcam image data (currenly np random, for testing purposes)
     return(eight_by_eight_grid)
 
 # LIVE TCAM PROCESS
 def live_tcam(bool,ip,socket):    
     # Create pipe to process 1 so that when p1 identifies the "end live feed" command, this process checks if this has been recieved each time round the loop. THIS WILL BE COMPLICATED!!!
     bool = True
-    while (bool==True):
+    while (bool==True):     # While still want stream running...
         # CALL GET TCAM HERE
-        # SEND JSON HERE
+        # SEND JSON HERE    form is {"STREAM": [8x8 matrix]}
         
         bool = toggle_q.get()
         if bool==False:
             acknowl = "Shutting down TCAM STREAM"
             print(acknowl)
             socket.sendto(acknowl,ip)
-
-        #listen for change in live cam True/False from p1. If now msg is {"STREAM":False}, stop this process PROPERLY! Very important - don't want memory leaks https://superfastpython.com/safely-stop-a-process-in-python/  
+            
+    # Write shutdown code here (outside loop)
+    # listen for change in live cam True/False from p1. If now msg is {"STREAM":False}, stop this process PROPERLY! Very important - don't want memory leaks https://superfastpython.com/safely-stop-a-process-in-python/  
         
 
 
@@ -34,12 +35,14 @@ def aocs_control(angle,speed,etc):
 
 # COMMAND PROCESS - this function might be best suited as the cmd process as can check if this is running directly from P1 and tell client "sorry mate, a command is already being executed!!!" 
 # The function below will be called when process 1 identifies the message as a command request. This func will then route the request to the specific command func above
-def parse_cmd(cmd_dict,cmmd_list):  # For additional intentifiable commands, add them to cmd_list and then add an extra elif to the parse_cmd() func
+def parse_cmd(cmd_dict,cmmd_list,ip,socket):  # For additional intentifiable commands, add them to cmd_list and then add an extra elif to the parse_cmd() func
     """Performs requested command and returns info on cmd success/failiure 
 
     Args:
         cmd_dict (string): decoded JSON string in form {"cmmd":[param1,param2,param3]} 
         cmd_list (list): hard-coded list of 4-character cmmd identifiers 
+        ip (string): ip address of client
+        socket (socket): server socket
     """
     # Extracting the requested cmmd and its params
     cmmd = list(cmmd.keys())[0]
@@ -63,13 +66,13 @@ def parse_cmd(cmd_dict,cmmd_list):  # For additional intentifiable commands, add
 
 # DATA PROCESS - P1 can check if this is running directly and tell client "sorry mate, a data request is already underway!!! Try again later..." 
 # The function below will be called when process 1 identifies the message as a data request. This func will then identify all data requests from True/False in the recieved JSON
-def parse_data(dat_req):
+def parse_data(dat_req,ip,socket):
     """Parses data request and returns single object (float, array, etc) containing requested data. This can then be stored in a dictionary.
 
     Args:
-        data (dict): dict of full data request in form {volt:True,curr:False,etc}
-        params (string): string comma list of params required for chosen data request
-        data_list (list): hard-coded string list of identifiable data requests
+        dat_req (dict): dict of full data request in dict form {"TCAM":True,"VOLT":False,"TEMP":True}
+        ip (string): ip address of client
+        socket (socket): server socket
     """
     
     data_out = {"TCAM":None,"VOLT":None,"TEMP":None} #  FILL FOR ALL DATAS IN DATA REQ LIST
@@ -77,15 +80,23 @@ def parse_data(dat_req):
     if dat_req["TCAM"] == True:
         tcam_data = get_tcam()  # get latest one-off tcam data from function
         data_out["TCAM"] = tcam_data
-    elif dat_req["VOLT"] == True:
+    if dat_req["VOLT"] == True:
         voltage_data = 5    # get latest voltage data (5 placeholder, for testing purposes)
         data_out["VOLT"] = voltage_data
+    if dat_req["TEMP"] == True:
+        temp_data = 25    # get latest voltage data (5 placeholder, for testing purposes)
+        data_out["TEMP"] = temp_data
 
     # add elifs for all datas!
     # add error catching to identify unidentified hiccups / errors and print + send msg to client saying what happened!
 
     # After the above, SEND data_out TO CLIENT
-        
+    
+    json_string = json.dumps(data_out)
+    print("data_out: ", json_string)
+    socket.sendto(json_string,ip)
+    print("Sent to ",ip)
+
 # JSON ENCODING INFO
 # Convert into JSON using json dumps, send to client using socket.sendto(msg,ip)?
 # https://stackoverflow.com/questions/42397511/python-how-to-get-json-object-from-a-udp-received-packet
